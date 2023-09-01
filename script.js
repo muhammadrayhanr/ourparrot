5a623249fd215e3d4ad8a1191d2971fbbc2be0bc
muhammadrayhanr <mrayhanr4869@gmail.com>
2023-09-01 14:05:03 +0700

    merapihkan

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Swipper Product
-var swiper = new Swiper('.mySwiper', {
-  grabCursor: false,
-  centeredSlides: false,
-  spaceBetween: 20,
-  slidesPerView: 'auto',
-});
+// var swiper = new Swiper('.mySwiper', {
+//   grabCursor: false,
+//   centeredSlides: false,
+//   spaceBetween: 20,
+//   slidesPerView: 'auto',
+// });
 
 // Back to Top Button
 //Get the button
@@ -28,4 +28,29 @@ mybutton.addEventListener('click', backToTop);
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// Slideshow
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('demo');
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' w3-opacity-off', '');
+  }
+  x[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' w3-opacity-off';
 }
